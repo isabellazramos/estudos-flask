1b12f9a536e77e7bdc3bb3f152b7235cbe55292d
Isabella Menezes Ramos <isabellamenezesramos@outlook.com>
2022-11-02 13:16:36 -0300

    adicionando exemplo de crud

--- exemplo-conceitos/12_crud/app.py
+++ exemplo-conceitos/12_crud/app.py
@@ -21,6 +21,31 @@ def index():
     estudantes = Estudante.query.all()
     return render_template('index.html', estudantes = estudantes)
 
+@app.route('/add', methods=['GET','POST'])
+def add():
+    if request.method == 'POST':
+        estudante = Estudante(request.form['nome'], request.form['idade'])
+        db.session.add(estudante)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('add.html')
+
+@app.route('/edit/<int:id>', methods=['GET', 'POST'])
+def edit(id):
+    estudante = Estudante.query.get(id)
+    if request.method == 'POST':
+        estudante.nome = request.form['nome']
+        estudante.idade = request.form['idade']
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('edit.html', estudante = estudante)    
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    estudante = Estudante.query.get(id)
+    db.session.delete(estudante)
+    db.session.commit()
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     db.create_all()
